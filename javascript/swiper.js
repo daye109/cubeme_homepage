57f0b4c34fa3aafbb79c940210dedb188e1dfb40
daye109 <daye109@nate.com>
2020-03-21 16:11:29 +0900

    change swiper

--- javascript/swiper.js
+++ javascript/swiper.js
@@ -1,29 +1,27 @@
-$(document).ready(function() {
-  new Swiper(".home_swiper_mobile", {
-    effect: "slide",
-    autoHeight: true,
-    speed: 1000,
-    slidesPerView: 1,
-    spaceBetween: -220,
-    slidesPerGroup: 1,
-    loop: false
-  });
+new Swiper(".home_swiper_mobile", {
+  effect: "slide",
+  autoHeight: true,
+  speed: 1000,
+  slidesPerView: 1,
+  spaceBetween: -220,
+  slidesPerGroup: 1,
+  loop: false
+});
 
-  new Swiper(".products_swiper_container", {
-    effect: "slide",
-    autoHeight: true,
-    speed: 1000,
-    slidesPerView: 3,
-    spaceBetween: 0,
-    slidesPerGroup: 1,
-    loop: false,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    }
-  });
+new Swiper(".products_swiper_container", {
+  effect: "slide",
+  autoHeight: true,
+  speed: 1000,
+  slidesPerView: 3,
+  spaceBetween: 0,
+  slidesPerGroup: 1,
+  loop: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
 });
